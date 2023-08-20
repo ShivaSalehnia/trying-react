c6281f3e12a813bba9faea15b8b6f2dd100f6b5e
Shiva Salehnia <shiva.salehnia@gmail.com>
2023-08-20 00:42:49 -0700

    Removing React logo

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <h2>Hello World</h2>
         <Weather city="Tehran" />
       </header>
