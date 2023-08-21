94bdc0ac15d3b3b6290d2e711c9cefc9205504ab
Shiva Salehnia <shiva.salehnia@gmail.com>
2023-08-21 01:12:59 -0700

    Link to Github
    Adding github link at the bottom of the code

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ export default function App() {
     <div className="App">
       <h1>Weather App</h1>
       <Weather />
+      <p>
+        This project is{" "}
+        <a href="https://github.com/ShivaSalehnia/trying-react">open source</a>,
+        by <a href="https://github.com/ShivaSalehnia">Shiva Salehnia</a>
+      </p>
     </div>
   );
 }
